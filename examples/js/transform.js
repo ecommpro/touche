d60f9f281a055bd3a390548f2f0127d6d2d6c47f
@manelio <manel.domenech@gmail.com>
2018-09-02 20:28:59 +0200

    add transform example

---
+++ examples/js/transform.js
@@ -0,0 +1,88 @@
+(function() {
+  
+touche.examples.push({
+
+title: 'Use your fingers, toes or nose',
+class: 'wide',
+
+go: function(el) {
+
+  var
+    scale,
+    rotation,
+    tx, ty,
+    cx, cy,
+    transform = touche.transform(),
+    rs = touche.transform(),
+    pivot = touche.transform().setId()
+  ;
+
+  el.style["touch-action"] = "none";
+
+  var shape = document.createElement('div');
+  shape.className = 'transformable';
+  el.appendChild(shape);
+
+
+  function refresh() {
+    rs
+      .setId()
+      .rotate(rotation)
+      .scale(scale)
+    ;
+
+    transform
+      .setId()
+      .translate(cx, cy)
+      .multiply(rs)
+      .translate(-cx, -cy)
+      .translate(tx, ty)
+      .multiply(pivot)
+    ;
+
+    shape.style['transform-origin'] = '0 0';
+    shape.style.transform = transform.toCSS3MatrixString();
+  }
+
+  touche(el)
+    .addGesture(touche.gestures.pan())
+    .addGesture(touche.gestures.pinch())
+    .addGesture(touche.gestures.rotate())
+    .on('touche.input', function(input) {
+      input.stopEvent();
+    })
+    .on('panmove', function(result, input, session, calculator) {
+      tx = input.deltaPosition.x;
+      ty = input.deltaPosition.y;
+    })  
+    .on('rotatemove', function(result, input, session, calculator) {
+      rotation = -input.deltaRotation;
+    })
+    .on('pinchmove', function(result, input, session, calculator) {
+      scale = input.deltaScale;
+    })
+    .on('touche.recentered', function(result, input, session) {
+      pivot.copy(transform);
+      scale = 1;
+      rotation = 0;
+      tx = 0;
+      ty = 0;
+    })
+    .on('touche.post', function(input) {
+      if (isNaN(input.centerX)) {
+        return;
+      }
+
+      var clientRect = el.getBoundingClientRect();
+      
+      cx = input.centerX - clientRect.left;
+      cy = input.centerY - clientRect.top;
+      refresh();
+    })
+    ;
+}
+
+})
+
+})()
+  
